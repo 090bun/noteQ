ba6eec11e40aa84eee18ffe1dd43fc2f7fb38f61
Aaron <chengfu0624@gmail.com>
2025-08-11 11:35:09 +0800

    feat:筆記頁問題一堆

--- frontend/my-app/src/app/note/page.jsx
+++ frontend/my-app/src/app/note/page.jsx
@@ -464,7 +464,7 @@ const handleAddNote = () => {
                                     <textarea 
                                         className={styles.modalTextarea}
                                         placeholder={
-                                            "請輸入筆記內容...\n範例格式：**\n- 粗體：**文字**\n- 斜體：*文字*\n- 標題：# ## ###\n- 列表：- 項目\n- 程式碼：`code`"
+                                            "請輸入筆記內容...\n範例格式：**\n- 粗體：**文字**\n- 斜體：*文字*\n- 標題：# ## ###\n- 列表：- 項目\n- 程式碼：`code`\n- 分隔線：---"
                                         }
                                         value={modalContent.split('\n---\n')[1] || ''}
                                         onChange={(e) => {
