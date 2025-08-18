c1af7632fc8e3f6e9b73420d62ca52d51f371137
bun <kasanomio@gmail.com>
2025-08-18 14:53:32 +0800

    feat: 更新 ECPay 設定，使用環境變數管理 URL 和商店資訊

--- backend-django/myapps/Ecpay/config.py
+++ backend-django/myapps/Ecpay/config.py
@@ -1,4 +1,8 @@
 # config.py
+import os
+
+REACT_BASE_URL = os.getenv("REACT_BASE_URL", "http://localhost:3000")
+DJANGO_BASE_URL = os.getenv("DJANGO_BASE_URL", "http://localhost:8000")
 
 class ECPayConfig:
     """綠界金流設定"""
@@ -7,20 +11,20 @@ class ECPayConfig:
     TEST_MODE = True
     
     # 測試商店資訊（綠界提供的測試參數）
-    MERCHANT_ID = '3002607'
-    HASH_KEY = 'pwFHCqoQZGmho4w6'
-    HASH_IV = 'EkRm7iFT261dpevs'
-    
+    MERCHANT_ID = os.getenv("MERCHANT_ID", '3002607')
+    HASH_KEY = os.getenv("HASH_KEY", 'pwFHCqoQZGmho4w6')
+    HASH_IV = os.getenv("HASH_IV", 'EkRm7iFT261dpevs')
+
     # API 網址（測試/正式）
     if TEST_MODE:
         ACTION_URL = 'https://payment-stage.ecpay.com.tw/Cashier/AioCheckOut/V5'
-        CLIENT_BACK_URL = 'http://localhost:3000/payment/success'  # 前端返回頁面，指向特定路由
+        CLIENT_BACK_URL = f'{REACT_BASE_URL}/user'  # 前端返回頁面，指向特定路由
     else:
         ACTION_URL = 'https://payment.ecpay.com.tw/Cashier/AioCheckOut/V5'
-        CLIENT_BACK_URL = 'https://your-production-frontend.com/payment/success'
+        CLIENT_BACK_URL = f'{REACT_BASE_URL}/user'
 
     # 回傳網址（ECPay server -> your server）
-    RETURN_URL = 'http://localhost:8000/ECpay-return/'
+    RETURN_URL = f'{DJANGO_BASE_URL}/ECpay-return/'
 
 config = ECPayConfig()
 
